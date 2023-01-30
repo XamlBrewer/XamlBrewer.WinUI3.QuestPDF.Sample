540451aa38400c0587e10f2db4e62268bd85d2f6
Diederik Krols <diederik_krols@hotmail.com>
2023-01-30 21:16:41 +0100

    ChartsPage scaffolding

---
+++ XamlBrewer.WinUI3.QuestPdf.Sample/Controls/ChartCanvas.cs
@@ -0,0 +1,39 @@
+﻿using Microcharts;
+using Microsoft.UI.Xaml;
+using SkiaSharp.Views.Windows;
+
+namespace XamlBrewer.WinUI.Controls
+{
+    /// <summary>
+    /// A SkiaSharp WinUI Canvas that draws a Microcharts Chart.
+    /// </summary>
+    public class ChartCanvas : SKXamlCanvas
+    {
+        public static readonly DependencyProperty ChartProperty =
+            DependencyProperty.Register(nameof(Chart), typeof(Chart), typeof(ChartCanvas), new PropertyMetadata(null));
+
+        public Chart Chart
+        {
+            get { return (Chart)GetValue(ChartProperty); }
+            set
+            {
+                SetValue(ChartProperty, value);
+                Invalidate();
+            }
+        }
+
+        protected override void OnPaintSurface(SKPaintSurfaceEventArgs e)
+        {
+            if (Chart == null)
+            {
+                e.Surface.Canvas.Clear();
+            }
+            else
+            {
+                Chart.DrawContent(e.Surface.Canvas, e.Info.Width, e.Info.Height);
+            }
+
+            base.OnPaintSurface(e);
+        }
+    }
+}
---
+++ XamlBrewer.WinUI3.QuestPdf.Sample/Views/ChartsPage.xaml.cs
@@ -0,0 +1,12 @@
+using Microsoft.UI.Xaml.Controls;
+
+namespace XamlBrewer.WinUI3.QuestPDF.Sample.Views
+{
+    public sealed partial class ChartsPage : Page
+    {
+        public ChartsPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
