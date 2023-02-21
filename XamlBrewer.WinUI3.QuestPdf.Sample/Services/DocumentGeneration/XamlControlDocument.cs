559b0cc049dcdd41806b7778ceeef5d70fb1d0e7
Diederik Krols <diederik_krols@hotmail.com>
2023-02-21 17:38:10 +0100

    XamlControls: RadialGauge

---
+++ XamlBrewer.WinUI3.QuestPdf.Sample/Services/DocumentGeneration/XamlControlDocument.cs
@@ -0,0 +1,53 @@
+﻿using QuestPDF.Drawing;
+using QuestPDF.Fluent;
+using QuestPDF.Infrastructure;
+using System.Collections.Generic;
+
+namespace XamlBrewer.WinUI3.QuestPDF.Sample.Services.DocumentGeneration
+{
+    internal class XamlControlDocument : IDocument
+    {
+        public List<byte[]> Model { get; }
+
+        public XamlControlDocument(List<byte[]> model)
+        {
+            Model = model;
+        }
+
+        public DocumentMetadata GetMetadata() => DocumentMetadata.Default;
+
+        public void Compose(IDocumentContainer container)
+        {
+            container.Page(page =>
+            {
+                page.Margin(40);
+
+                page.Header()
+                    .PaddingBottom(20)
+                    .Text("XAML Brewer QuestPDF XAML Controls Sample")
+                    .FontSize(16);
+
+                page.Content().Element(ComposeBody);
+
+                page.Footer().Text(text =>
+                {
+                    text.Span("page ");
+                    text.CurrentPageNumber();
+                });
+            });
+        }
+
+        private void ComposeBody(IContainer body)
+        {
+            body.Column(column =>
+            {
+                foreach (var xamlControl in Model)
+                {
+                    column.Item()
+                        .Height(100)
+                        .Image(xamlControl, ImageScaling.FitHeight);
+                }
+            });
+        }
+    }
+}
--- XamlBrewer.WinUI3.QuestPdf.Sample/Views/XamlControlPage.xaml.cs
+++ XamlBrewer.WinUI3.QuestPdf.Sample/Views/XamlControlPage.xaml.cs
@@ -1,13 +1,15 @@
-﻿using Microsoft.UI;
+﻿using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Media.Imaging;
+using QuestPDF.Fluent;
 using System;
-using System.IO;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using Windows.Graphics.Imaging;
-using Windows.Storage;
 using Windows.Storage.Streams;
+using XamlBrewer.WinUI3.QuestPDF.Sample.Services.DocumentGeneration;
 
 namespace XamlBrewer.WinUI3.QuestPDF.Sample.Views
 {
@@ -20,31 +22,63 @@ namespace XamlBrewer.WinUI3.QuestPDF.Sample.Views
 
         private async void XamlControlButton_Click(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
         {
-            // Test: transform XAML visual to image.
+            var filePath = "C:\\Temp\\xamlcontrols.pdf";
 
+            //if (ActualTheme == ElementTheme.Dark)
+            //{
+            //    ApplyTheme(ElementTheme.Light);
+            //}
+
+            var document = new XamlControlDocument(
+                new List<byte[]> {
+                    await CreateBytes(RadialGauge)
+                });
+
+            document.GeneratePdf(filePath);
+
+            //if (ActualTheme == ElementTheme.Dark)
+            //{
+            //    ApplyTheme(ElementTheme.Dark);
+            //}
+
+            var process = new Process
+            {
+                StartInfo = new ProcessStartInfo(filePath)
+                {
+                    UseShellExecute = true
+                }
+            };
+
+            process.Start();
+        }
+
+        private async Task<byte[]> CreateBytes(UIElement control)
+        {
+            // Get XAML Visual in BGRA8 format
             var rtb = new RenderTargetBitmap();
-            await rtb.RenderAsync(RadialGauge, (int)RadialGauge.DesiredSize.Width, (int)RadialGauge.DesiredSize.Height);
+            await rtb.RenderAsync(RadialGauge, (int)control.DesiredSize.Width, (int)control.DesiredSize.Height);
+
+            // Apply to test image
             TestImage.Source = rtb;
 
-            var pixelBuffer = await rtb.GetPixelsAsync();
-
-            var stream = File.Create("C:\\Temp\\bitmap.png");
-            stream.Flush();
-            stream.Close();
-
-            using IRandomAccessStream fileStream = await FileRandomAccessStream.OpenAsync("C:\\Temp\\bitmap.png", FileAccessMode.ReadWrite);
-            var encoder = await BitmapEncoder.CreateAsync(BitmapEncoder.PngEncoderId, fileStream);
-
+            // Encode as PNG
+            var pixelBuffer = (await rtb.GetPixelsAsync()).ToArray();
+            IRandomAccessStream mraStream = new InMemoryRandomAccessStream();
+            var encoder = await BitmapEncoder.CreateAsync(BitmapEncoder.PngEncoderId, mraStream);
             encoder.SetPixelData(
                 BitmapPixelFormat.Bgra8,
                 BitmapAlphaMode.Premultiplied,
                 (uint)rtb.PixelWidth,
                 (uint)rtb.PixelHeight,
-                96,
-                96,
-                pixelBuffer.ToArray());
-
+                192,
+                192,
+                pixelBuffer);
             await encoder.FlushAsync();
+
+            // Transform to byte array
+            var bytes = new byte[mraStream.Size];
+            await mraStream.ReadAsync(bytes.AsBuffer(), (uint)mraStream.Size, InputStreamOptions.None);
+            return bytes;
         }
     }
 }
